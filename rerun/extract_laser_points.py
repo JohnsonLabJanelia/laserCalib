dfcd377e24d5d3dffad803d246c1c175cfe50e99
Jinyao Yan <lingerlyn@gmail.com>
2024-06-23 13:37:12 -0400

    use config file

---
+++ rerun/extract_laser_points.py
@@ -0,0 +1,109 @@
+import os
+import numpy as np
+import pickle as pkl
+import argparse
+import glob
+from feature_detection import *
+from multiprocessing import Pool
+from tqdm import tqdm
+import cv2 as cv
+import time 
+from tqdm.contrib.concurrent import process_map
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', type=str, required=True)
+parser.add_argument('-i', '--dataset_idx', type=int, required=False, default=0)
+
+args = parser.parse_args()
+
+with open(args.config, 'r') as f:
+    calib_config = json.load(f)
+dataset_idx = args.dataset_idx
+
+root_dir = calib_config['root_dir']
+laser_datasets = calib_config['lasers']
+
+cam_names = []
+one_dataset_dir = os.path.join(root_dir + laser_datasets[0])
+for file in glob.glob(one_dataset_dir + "/*.mp4"):
+    file_name = file.split("/")
+    cam_names.append(file_name[-1].split('.')[0])
+cam_names.sort()
+n_cams = len(cam_names)
+print("Number of cameras: ", n_cams)
+
+def extract_laser_points_per_camera(dataset_camera_idx):
+    dataset_idx, camera_idx = dataset_camera_idx
+    video_file = os.path.join(root_dir, "{}/".format(laser_datasets[dataset_idx]) + cam_names[camera_idx] + ".mp4")
+    output_file = os.path.join(root_dir, "results/{}/".format(laser_datasets[dataset_idx]) + cam_names[camera_idx] + "_centroids.pkl")
+    vr = cv.VideoCapture(video_file)
+
+    centroids = np.zeros((frame_end - frame_start, 2), dtype=float)
+    centroids[:] = np.nan
+
+    count = 0
+
+    if frame_start != 0:
+        vr.set(cv.CAP_PROP_POS_FRAMES, frame_start)
+
+    for i in range(frame_start, frame_end):
+        ret, frame = vr.read()
+        if not ret:
+            break
+        laser_coord = green_laser_finder_faster(frame, 50)
+        if laser_coord:
+            centroids[i] = laser_coord
+            count = count + 1
+
+    with open(output_file, 'wb') as f:
+        pkl.dump(centroids, f)
+    
+    return camera_idx, count
+
+
+if __name__ == '__main__':
+
+    if dataset_idx != 0:
+        print(laser_datasets[dataset_idx])
+        frame_start = calib_config['frames'][dataset_idx][0]
+        frame_end = calib_config['frames'][dataset_idx][1]
+
+        start_time = time.time()
+        # make folder 
+        results_dir = os.path.join(root_dir, 'results')
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+
+        laser_points_folder =  os.path.join(results_dir, laser_datasets[dataset_idx])
+        if not os.path.exists(laser_points_folder):
+            os.makedirs(laser_points_folder)
+
+        dataset_camera_idx = [[dataset_idx, cam_idx] for cam_idx in range(n_cams)]
+        results = process_map(extract_laser_points_per_camera, dataset_camera_idx, max_workers=4)
+        end_time = time.time()
+        for cam_idx, num_frames in results:
+            print(cam_names[cam_idx] + ": {}".format(num_frames))
+        print("time elapsed: {:.2f} second".format(end_time - start_time))
+    else:
+        for dataset_idx in range(len(laser_datasets)):
+            print(laser_datasets[dataset_idx])
+            frame_start = calib_config['frames'][dataset_idx][0]
+            frame_end = calib_config['frames'][dataset_idx][1]
+
+            start_time = time.time()
+            # make folder 
+            results_dir = os.path.join(root_dir, 'results')
+            if not os.path.exists(results_dir):
+                os.makedirs(results_dir)
+
+            laser_points_folder =  os.path.join(results_dir, laser_datasets[dataset_idx])
+            if not os.path.exists(laser_points_folder):
+                os.makedirs(laser_points_folder)
+
+            dataset_camera_idx = [[dataset_idx, cam_idx] for cam_idx in range(n_cams)]
+            results = process_map(extract_laser_points_per_camera, dataset_camera_idx, max_workers=4)
+            end_time = time.time()
+            for cam_idx, num_frames in results:
+                print(cam_names[cam_idx] + ": {}".format(num_frames))
+            print("time elapsed: {:.2f} second".format(end_time - start_time))
---
+++ rerun/tutorial.py
@@ -0,0 +1,55 @@
+import rerun as rr
+
+from math import tau
+import numpy as np
+from rerun.utilities import build_color_spiral
+from rerun.utilities import bounce_lerp
+
+rr.init("rerun_example_dna_abacus")
+rr.spawn()
+rr.set_time_seconds("stable_time", 0)
+
+NUM_POINTS = 100
+
+# Points and colors are both np.array((NUM_POINTS, 3))
+points1, colors1 = build_color_spiral(NUM_POINTS)
+points2, colors2 = build_color_spiral(NUM_POINTS, angular_offset=tau*0.5)
+
+rr.log("dna/structure/left", rr.Points3D(points1, colors=colors1, radii=0.08))
+rr.log("dna/structure/right", rr.Points3D(points2, colors=colors2, radii=0.08))
+
+rr.log(
+    "dna/structure/scaffolding",
+    rr.LineStrips3D(np.stack((points1, points2), axis=1), colors=[128, 128, 128])
+)
+
+offsets = np.random.rand(NUM_POINTS)
+beads = [bounce_lerp(points1[n], points2[n], offsets[n]) for n in range(NUM_POINTS)]
+colors = [[int(bounce_lerp(80, 230, offsets[n] * 2))] for n in range(NUM_POINTS)]
+rr.log(
+    "dna/structure/scaffolding/beads",
+    rr.Points3D(beads, radii=0.06, colors=np.repeat(colors, 3, axis=-1)),
+)
+
+
+time_offsets = np.random.rand(NUM_POINTS)
+
+for i in range(400):
+    time = i * 0.01
+    rr.set_time_seconds("stable_time", time)
+
+    times = np.repeat(time, NUM_POINTS) + time_offsets
+    beads = [bounce_lerp(points1[n], points2[n], times[n]) for n in range(NUM_POINTS)]
+    colors = [[int(bounce_lerp(80, 230, times[n] * 2))] for n in range(NUM_POINTS)]
+    rr.log(
+        "dna/structure/scaffolding/beads",
+        rr.Points3D(beads, radii=0.06, colors=np.repeat(colors, 3, axis=-1)),
+    )
+
+for i in range(400):
+    time = i * 0.01
+    rr.set_time_seconds("stable_time", time)
+    rr.log(
+        "dna/structure",
+        rr.Transform3D(rotation=rr.RotationAxisAngle(axis=[0, 0, 1], radians=time / 4.0 * tau)),
+    )
